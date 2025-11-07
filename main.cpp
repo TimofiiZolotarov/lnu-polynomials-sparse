35a04042573297960805a4d85b744ca65a314670
Eugene <eugeneyemets@gmail.com>
2025-11-07 16:40:50 +0200

    Update1 main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,36 @@
 #include "utils.h"
+#include <iostream>
 using namespace std;
 
 int main() {
-    // створюємо порожній поліном
+    // P(x) = 5x^2 + 3
     Poly P = make_empty_poly();
+    insert_mono(P, 5.0, 2);
+    insert_mono(P, 3.0, 0);
 
-    // пробуємо "вставити" щось (поки що функція пуста, але вона є)
-    insert_mono(P, 5.0, 2); // 5x^2
-    insert_mono(P, 3.0, 0); // +3
+    // Q(x) = -x + 4
+    Poly Q = make_empty_poly();
+    insert_mono(Q, -1.0, 1);
+    insert_mono(Q,  4.0, 0);
+    cout << "P(x) = "; print_poly(P); cout << "\n";
+    cout << "Q(x) = "; print_poly(Q); cout << "\n";
+    cout << "P(2) = " << value(P, 2.0) << "\n";
 
-    cout << "Polynomial(x) = ";
-    print_poly(P);
-    cout << endl;
+    Poly S  = add(P, Q);
+    Poly D  = sub(P, Q);
+    Poly M  = mul(P, Q);
+    Poly P3 = power(P, 3);
 
-    double res = value(P, 2.0);
-    cout << "Result: " << res << endl;
+    cout << "P + Q = "; print_poly(S);  cout << "\n";
+    cout << "P - Q = "; print_poly(D);  cout << "\n";
+    cout << "P * Q = "; print_poly(M);  cout << "\n";
+    cout << "P^3   = "; print_poly(P3); cout << "\n";
 
-    // обов'язково очищаємо
     clear_poly(P);
-
+    clear_poly(Q);
+    clear_poly(S);
+    clear_poly(D);
+    clear_poly(M);
+    clear_poly(P3);
     return 0;
 }
