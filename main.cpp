b12e8b7a868990adf400199ca5e72d70e3413245
TimofiiZolotarov <Tymofii.Zolotarov@lnu.edu.ua>
2025-11-25 10:05:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,28 @@ Poly toPoly(const Polynomial& src) {
     return res;
 }
 
+void print_menu() {
+    cout << "\n============================================================\n";
+    cout << "                 ОПЕРАЦІЇ З ПОЛІНОМАМИ\n";
+    cout << "============================================================\n";
+    cout << " 1)  Додавання            :   P1(x) + P2(x)\n";
+    cout << " 2)  Віднімання           :   P1(x) - P2(x)\n";
+    cout << " 3)  Множення             :   P1(x) *  P2(x)\n";
+    cout << " 4)  Ділення в стовпчик   :   P1(x) /  P2(x)\n";
+    cout << " 5)  Обчислити P1(x0)\n";
+    cout << " 6)  Похідна P1(x)\n";
+    cout << " 7)  Піднесення P1(x) до степеня k\n";
+    cout << "------------------------------------------------------------\n";
+    cout << " 0)  ВИХІД\n";
+    cout << "============================================================\n";
+    cout << "Ваш вибір: ";
+}
+
+void flushInput() {
+    cin.clear();
+    while (cin.get() != '\n');
+}
+
 int main() {
     Polynomial P1, P2;
 
@@ -27,7 +49,7 @@ int main() {
 
     int mode;
     cin >> mode;
-    cin.ignore();
+    flushInput();
 
     // зчитування
     if (mode == 1) {
@@ -50,61 +72,75 @@ int main() {
     cout << "\nP1(x) = "; P1.print();
     cout << "P2(x) = "; P2.print();
 
-    // конвертація у формат worms.h
+    // конвертація у формат utils.h
     Poly A = toPoly(P1);
     Poly B = toPoly(P2);
 
     // меню
     while (true) {
-        cout << "\n1 - додавання\n"
-                "2 - віднімання\n"
-                "3 - множення\n"
-                "4 - ділення в стовпчик\n"
-                "5 - значення P1(x0)\n"
-                "6 - похідна P1\n"
-                "7 - P1^k\n0 - вихід\n"
-                "Ваш вибір: ";
+        print_menu();
 
         int op;
-        cin >> op;
+        if (!(cin >> op)) {
+            flushInput();
+            cout << "Будь ласка, введіть число від 0 до 7.\n";
+            continue;
+        }
+        flushInput();
 
-        if (op == 0) break;
+        if (op == 0) {
+            cout << "Вихід...\n";
+            break;
+        }
+
+        cout << "------------------------------------------------------------\n";
 
         switch (op) {
             case 1: {
+                cout << "P1(x) + P2(x) = ";
                 Poly R = add(A, B);
                 print_poly(R); cout << "\n";
                 clear_poly(R);
                 break;
             }
             case 2: {
+                cout << "P1(x) - P2(x) = ";
                 Poly R = sub(A, B);
                 print_poly(R); cout << "\n";
                 clear_poly(R);
                 break;
             }
             case 3: {
+                cout << "P1(x) * P2(x) = ";
                 Poly R = mul(A, B);
                 print_poly(R); cout << "\n";
                 clear_poly(R);
                 break;
             }
             case 4: {
-                if (is_zero(B)) { cout << "Ділення в стовпчик неможливе\n"; break; }
+                if (is_zero(B)) {
+                    cout << "Ділення неможливе: P2(x) = 0\n";
+                    break;
+                }
                 Poly Q = make_empty_poly(), R = make_empty_poly();
                 div(A, B, Q, R);
                 cout << "Q(x) = "; print_poly(Q);
                 cout << "\nR(x) = "; print_poly(R); cout << "\n";
-                clear_poly(Q); clear_poly(R);
+                clear_poly(Q);
+                clear_poly(R);
                 break;
             }
             case 5: {
                 double x;
-                cout << "x0 = "; cin >> x;
+                cout << "x0 = ";
+                cin >> x;
+                flushInput();
+
                 cout << "P1(" << x << ") = " << value(A, x) << "\n";
                 break;
             }
             case 6: {
+                cout << "Похідна P1(x): ";
                 Poly D = derivative(A);
                 print_poly(D); cout << "\n";
                 clear_poly(D);
@@ -112,7 +148,15 @@ int main() {
             }
             case 7: {
                 int k;
-                cout << "k = "; cin >> k;
+                cout << "k = ";
+                cin >> k;
+                flushInput();
+
+                if (k < 0) {
+                    cout << "k має бути >= 0\n";
+                    break;
+                }
+
                 Poly R = power(A, k);
                 print_poly(R); cout << "\n";
                 clear_poly(R);
